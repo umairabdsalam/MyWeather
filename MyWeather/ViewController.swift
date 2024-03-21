cff22d68e58bdfe063c3c29e5df8c42ca68b9984
Umair Abdus Salam <umairabdsalam@gmail.com>
2024-03-21 21:53:53 +0000

    Initial Commit

--- MyWeather/ViewController.swift
+++ MyWeather/ViewController.swift
@@ -7,6 +7,12 @@
 
 import UIKit
 
+// Location:  Core Location
+// tableView
+// custom cell: collection view
+// API request to get the data
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
